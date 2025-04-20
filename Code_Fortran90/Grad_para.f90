2bcbe3434de50dcbfb36c5343b1c8c59c3099317
AbdallahZerfaoui <82400944+AbdallahZerfaoui@users.noreply.github.com>
2025-04-20 16:54:29 +0200

    Refactor Grad_para.f90 and add Makefile for build automation

--- Code_Fortran90/Grad_para.f90
+++ Code_Fortran90/Grad_para.f90
@@ -8,7 +8,7 @@ contains
     include "mpif.h"
 
 !============================================
-!Déclaration des variables
+!Dï¿½claration des variables
 !============================================
     integer, intent(in) :: n,Nx
 	double precision,dimension(1:5,i1:ie), intent(in) :: A
@@ -20,7 +20,7 @@ contains
 	double precision:: t1_b,t2_b,t3_b,t1_e,t2_e,t3_e
   	double precision :: alpha_me_num,alpha_me_deno,alpha_num=0,alpha_deno=0,beta=1,&
   	beta_me_num,beta_me_deno,beta_num=0,beta_deno=0, norm_Gr_loc, norm_Gr=1.d0
-	integer::compt,l,Me,i1,ie, statinfo, Np
+	integer::compt=0,l,Me,i1,ie, statinfo, Np
 	
 !==============================
 ! Initialisation de l'algorithme
@@ -30,7 +30,7 @@ Gr(i1:ie)=prodMV_para(A,U,n,Nx,i1,ie)-1.d0*F(i1:ie)
 DIR(i1:ie)= -Gr(i1:ie)
 
 !==============================
-!Début de l'alogorithme
+!Dï¿½but de l'alogorithme
 !==============================
 !Condition d'arret de l'algorithme
 do while (norm_Gr>Epsi)
@@ -38,7 +38,7 @@ do while (norm_Gr>Epsi)
 !==============================
 !Communication du vecteur de direction
 !==============================
-!Communication des blocs de Nx suivants la partie concerné 
+!Communication des blocs de Nx suivants la partie concernï¿½ 
 !sauf en pour Me=Np-1	
 if (Me<Np-1) then !suivant
 		
@@ -47,7 +47,7 @@ if (Me<Np-1) then !suivant
 		
 	end if
 	
-!Communication des blocs de Nx précédents la partie concerné 
+!Communication des blocs de Nx prï¿½cï¿½dents la partie concernï¿½ 
 !sauf en pour Me=0	
 	if (Me>0) then 	
 		call MPI_SENDRECV(DIR(i1:i1+Nx-1), Nx, MPI_DOUBLE_PRECISION, Me-1, 101, DIR(i1-Nx:i1-1), &
@@ -62,7 +62,7 @@ V=prodMV_para(A,DIR,n,Nx,i1,ie)
 !==============================
 ! Calcul de alpha
 !==============================
-!Calcul de la partie numérateur	
+!Calcul de la partie numerateur	
 alpha_me_num=dot_product(Gr(i1:ie),Gr(i1:ie))
 !Calcul de partie  denominateur	
 alpha_me_deno=dot_product(DIR(i1:ie),V(i1:ie)) 
@@ -70,7 +70,7 @@ alpha_me_deno=dot_product(DIR(i1:ie),V(i1:ie))
 !Envoi et sommation des parties numerateurs de chaque processeur	
 call MPI_ALLREDUCE(alpha_me_num,alpha_num,1,MPI_DOUBLE_PRECISION,MPI_SUM,MPI_COMM_WORLD,statinfo)
 	
-!Envoi et sommation des parties dénominateurs de chaque processeur
+!Envoi et sommation des parties denominateurs de chaque processeur
     call MPI_ALLREDUCE(alpha_me_deno,alpha_deno,1,MPI_DOUBLE_PRECISION,MPI_SUM,MPI_COMM_WORLD,statinfo)
 	
 alpha=alpha_num/alpha_deno
@@ -92,20 +92,20 @@ U(i1:ie)=U(i1:ie)+alpha*DIR(i1:ie)
 !==============================
 ! Calcul de beta
 !==============================
-!Calcul de la partie numérateur
+!Calcul de la partie numï¿½rateur
 beta_me_num = dot_product(Gr(i1:ie),Gr(i1:ie))
-!Calcul de la partie dénominateur
+!Calcul de la partie dï¿½nominateur
 beta_me_deno = dot_product(Grk(i1:ie),Grk(i1:ie))
 	
 !Envoie et sommation des parties numerateurs de chaque processeur
 	call MPI_ALLREDUCE(beta_me_num,beta_num,1,MPI_DOUBLE_PRECISION,MPI_SUM,MPI_COMM_WORLD,statinfo)
 	
-!Envoie et sommation des parties dénominateurs de chaque processeur
+!Envoie et sommation des parties dï¿½nominateurs de chaque processeur
 	call MPI_ALLREDUCE(beta_me_deno,beta_deno,1,MPI_DOUBLE_PRECISION,MPI_SUM,MPI_COMM_WORLD,statinfo)
 	
 	beta=beta_num/beta_deno
 
-!mise à jour de la direction
+!mise ï¿½ jour de la direction
 	DIR(i1:ie)=-1.d0*Gr(i1:ie)+beta*DIR(i1:ie)
     
 !%==================================
