d6eea43dbeddb232675bfdd9c61d2309c6663146
AbdallahZerfaoui <zerfaouiabdallah@gmail.com>
2025-04-27 21:36:39 +0000

    Fixes typo in config loading method
    Corrects a typo in the configuration loading method name,
    ensuring that the configuration is loaded correctly.
    
    Adds *.pyc to .gitignore

--- HES_python_version/config.py
+++ HES_python_version/config.py
@@ -4,7 +4,7 @@ class ConfigLoader():
     def __init__(self, filename = "data.json"):
         self.filename = filename
 
-    def laod_config(self) -> dict:
+    def load_config(self) -> dict:
         with open(self.filename, 'r') as f:
             config = json.load(f)
         return config
