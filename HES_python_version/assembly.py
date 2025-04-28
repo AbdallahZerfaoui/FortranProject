b5c03e2222219347969b6e26e8aaa9b17614d8e8
AbdallahZerfaoui <zerfaouiabdallah@gmail.com>
2025-04-28 13:55:49 +0000

    Updates global 1D index range
    Adjusts the range of global 1D indexes to ensure that it correctly reflects the problem size.
    
    Modifies how Nx is accessed within the grid configuration.

--- HES_python_version/assembly.py
+++ HES_python_version/assembly.py
@@ -10,8 +10,8 @@ class ProblemAssembler:
         Fill the right-hand side vector F based on the problem definition.
         """
         _problem_size = self._grid.n
-        _Nx = self._grid.Nx
-        global_1D_indexes = np.arange(1, _problem_size, dtype=int)
+        _Nx = self._grid.config["Nx"]
+        global_1D_indexes = np.arange(1, _problem_size + 1, dtype=int) #TODO: check if it is 0 to size or 1 to size + 1
         local_i_indexes = global_1D_indexes % _Nx
         local_j_indexes = global_1D_indexes // _Nx
         
